5e162cf16bd400bd3696d6ff860408e99500ce95
Ruri <ruridev@protonmail.com>
2022-08-20 19:06:31 +0200

    Added examples to work with images

--- OB2TestPlugin/Methods.cs
+++ OB2TestPlugin/Methods.cs
@@ -1,7 +1,9 @@
 ﻿using CalculatorLib;
 using RuriLib.Attributes;
 using RuriLib.Logging;
+using RuriLib.Models.Blocks.Settings;
 using RuriLib.Models.Bots;
+using System;
 
 namespace OB2TestPlugin.Blocks.Functions
 {
@@ -16,6 +18,38 @@ namespace OB2TestPlugin.Blocks.Functions
             data.Logger.LogHeader();
             data.Logger.Log($"Calculated: {firstNumber} + {secondNumber} = {sum}", LogColors.YellowGreen);
             return sum;
+        }
+    }
+}
+
+namespace OB2TestPlugin.Blocks.Images
+{
+    [BlockCategory("Images", "Blocks that allow to work with images", "#ffae42")]
+    public static class Methods
+    {
+        [BlockAction("ImageTest")]
+        public static void SayHello(RuriLib.Models.Blocks.BlockInstance block)
+        {
+            block.Settings["str"].InputMode = SettingInputMode.Fixed;
+            block.GetFixedSetting<StringSetting>("str").Value = "edited";
+        }
+
+        [BlockAction("ImageTest")]
+        public static void SayGoodbye(RuriLib.Models.Blocks.BlockInstance block)
+        {
+            var imageSize = block.Descriptor.Images["myImage"].Value?.Length ?? 0;
+            block.Settings["size"].InputMode = SettingInputMode.Fixed;
+            block.GetFixedSetting<IntSetting>("size").Value = imageSize;
+            (block as RuriLib.Models.Blocks.AutoBlockInstance).Safe = true;
+        }
+
+        [Block("Image attribute test")]
+        [BlockImage("myImage")]
+        public static void ImageTest(BotData data, string str, int size)
+        {
+            data.Logger.LogHeader();
+            data.Logger.Log($"The string says: {str}", LogColors.YellowOrange);
+            data.Logger.Log($"The image size is: {size}", LogColors.YellowOrange);
         }
     }
 }
